78a7b76b89980addcab74638bfcfeb3536710e6c
Danyal Faheem <danyalfaheem@gmail.com>
2022-12-08 18:03:21 +0500

    Updates

---
+++ precache-manifest.cf35024939926aacf0c9ca8660aa6df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbfd81f6d0e8e770828dc39b8e177c2",
+    "url": "/DanyalFaheem.github.io/index.html"
+  },
+  {
+    "revision": "663e3b4abb3dacff8d0c",
+    "url": "/DanyalFaheem.github.io/static/css/main.3a54c065.chunk.css"
+  },
+  {
+    "revision": "b6e14c980023522e28d2",
+    "url": "/DanyalFaheem.github.io/static/js/2.23b81fc6.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/DanyalFaheem.github.io/static/js/2.23b81fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663e3b4abb3dacff8d0c",
+    "url": "/DanyalFaheem.github.io/static/js/main.e4f3411c.chunk.js"
+  },
+  {
+    "revision": "81c2eda1713a15a803c5",
+    "url": "/DanyalFaheem.github.io/static/js/runtime-main.58b5db94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pizza/precache-manifest.8ab8cf1db9b7f7827bd9d29ac762b13c.js"
+  "/DanyalFaheem.github.io/precache-manifest.cf35024939926aacf0c9ca8660aa6df9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pizza/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DanyalFaheem.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
