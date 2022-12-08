a12f1312972d8949ec36c04dd06327e284ee3d63
Danyal Faheem <danyalfaheem@gmail.com>
2022-12-08 17:56:51 +0500

    Updates

---
+++ precache-manifest.8ab8cf1db9b7f7827bd9d29ac762b13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae663ae4b6b2465c1fe22df9d906f08",
+    "url": "/Pizza/index.html"
+  },
+  {
+    "revision": "663e3b4abb3dacff8d0c",
+    "url": "/Pizza/static/css/main.3a54c065.chunk.css"
+  },
+  {
+    "revision": "b6e14c980023522e28d2",
+    "url": "/Pizza/static/js/2.23b81fc6.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/Pizza/static/js/2.23b81fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663e3b4abb3dacff8d0c",
+    "url": "/Pizza/static/js/main.e4f3411c.chunk.js"
+  },
+  {
+    "revision": "2417c5f97f3078fe28cf",
+    "url": "/Pizza/static/js/runtime-main.ae61fabc.js"
+  }
+]);
